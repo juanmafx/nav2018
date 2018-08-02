2aecb7e5ad515f735605f6ba83dcb1eda4c9c961
Juan Manuel Ramallo <jramallo1989@gmail.com>
2018-08-02 11:15:58 -0300

    kjl
    jklkjl

---
+++ sedos.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="es">
+<head>
+</head>
+<body>
+<?php require ("nav.php") ?>
+<main role="main" class="container">
+
+<div class="jumbotron">
+<h1>Inicio:</h1>
+ 
+</div>
+
+
+
+
+</main>
+
+</body></html>
